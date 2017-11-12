ae24eb5233afac01fb40ecb5c6943fba48b20254
Paul Loong <lxgouc@foxmail.com>
2017-11-12 20:15:04 +0800

    fix sth

--- new_tcpclient/pthread_xxx.h
+++ new_tcpclient/pthread_xxx.h
@@ -8,6 +8,6 @@ void *pthread_xxx(void *arg);
 bool rpifromarduino(int&, char*);
 void rpitodeepin(int, const char*);
 void *rpifromdeepin(void *arg);
-void rpitoarduino(char*, int&);
+void rpitoarduino(char*, const int&);
 
 #endif
